97805cdc031c8504ef6c698c30397fd412709724
uu <giladbarkan@live.com>
2016-06-08 14:23:38 -0400

    passed tests

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,10 @@
 # Implement your object-oriented solution here!
+class SumSquareDifference
+  def initialize(n)
+    @n = n
+  end
+
+  def difference
+    (@n * (@n + 1) / 2) * (@n * (@n + 1) / 2) - @n * (@n + 1) * (2*@n + 1) / 6
+  end
+end
--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,5 @@
 # Implement your procedural solution here!
+def sum_square_difference(n)
+    (n * (n + 1) / 2) * (n * (n + 1) / 2) - n * (n + 1) * (2*n + 1) / 6
+end
+
